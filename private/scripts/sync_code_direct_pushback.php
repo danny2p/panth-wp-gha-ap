1fe48aed3c267de3f40b35e838667fde98b0aa35
Danny P <danny.pfeiffer@pantheon.io>
2023-04-27 00:06:43 -0600

    switching to termimnus build tools deployments. Clean up quicksilver pushback

--- private/scripts/sync_code_direct_pushback.php
+++ private/scripts/sync_code_direct_pushback.php
@@ -8,6 +8,8 @@
 * IMPORTANT. This script expects that you have used the 
 * terminus secrets plugin to set secrets as outined below 
 *
+* if not using terminus secrets, manually create /files/private/secrets.json
+*
 * To store and fetch we will use the terminus secrets plugin
 * You’ll need your site id or machine name handy
 * terminus secrets:set {site_id}.dev token ghp_xxxxxxxxxxxxxxxx
@@ -16,38 +18,40 @@
 *
 */
 
-function load_git_secrets($gitSecretsFile) {
-  if (!file_exists($gitSecretsFile)) {
-    print "Could not find $gitSecretsFile\n";
+function load_git_secrets($git_secrets_file) {
+  if (!file_exists($git_secrets_file)) {
+    print "Could not find $git_secrets_file\n";
     return [];
   }
-  $gitSecretsContents = file_get_contents($gitSecretsFile);
-  if (empty($gitSecretsContents)) {
+  $git_secrets_content = file_get_contents($git_secrets_file);
+  if (empty($git_secrets_content)) {
     print "GitHub secrets file is empty\n";
     return [];
   }
-  $gitSecrets = json_decode($gitSecretsContents, true);
-  if (empty($gitSecrets)) {
+  $git_secrets = json_decode($git_secrets_content, true);
+  if (empty($git_secrets)) {
     print "No data in Git secrets\n";
   }
-  return $gitSecrets;
+  return $git_secrets;
 }
 
 /*
-*  We only want this firing on Dev for this case (ie: when code is merged to master)
+*  If we only want this firing on Dev, or a specific branch (ie: when code is merged to master), use a check like below
 */ 
 
+/*
 if (!isset($_ENV['PANTHEON_ENVIRONMENT']) || $_ENV['PANTHEON_ENVIRONMENT'] != "dev" ) {
     return;
 }
+*/
 
-$bindingDir = $_SERVER['HOME'];
-$fullRepository = realpath("$bindingDir/code");
-$privateFiles = realpath("$bindingDir/files/private");
-$gitSecretsFile = "$privateFiles/secrets.json";
-$gitSecrets = load_git_secrets($gitSecretsFile);
-$git_token = $gitSecrets['token'];
-$git_remote = $gitSecrets['git_remote'];
+
+
+
+$private_files = realpath($_SERVER['HOME']."/files/private");
+$git_secrets_file = "$private_files/.build-secrets/tokens.json";
+$git_secrets = load_git_secrets($git_secrets_file);
+$git_token = $git_secrets['token'];
 $auth_string = "$git_token@github.com";
 
 // since we only asked for the https path and token we need to rebuild the url
@@ -64,37 +68,52 @@ if (empty($git_token)) {
 
 /*
 *
-* Let's check for changes in Github
-*
-* Since Pantheon is really authoritative, as we're running the code, we'll try to automatically
-* push back to Github master.  In most cases this should be safe if commits to Github master
+* Since Pantheon is really authoritative, in the sense that we're running the code, 
+* we'll try to automatically push back to Github master.
+* In most cases this should be safe if commits to Github master
 * branch are always being pushed to Pantheon.
+* extend this logic as necessary to fit your needs.
 *
 */
 
-// latest local commit
-$local = exec("git rev-parse @");
-// latest github commit
-$remote = exec('git ls-remote '.$git_remote_auth.' | head -1 | sed "s/HEAD//"');
-// check if Pantheon's latest commit to master is a descendent of latest commit in github - if not we probably don't want to push
-$is_remote_ancestor = exec("git merge-base --is-ancestor $remote master");
-$ancestor_output = $is_remote_ancestor ? 'Github HEAD is not ancestor' : 'Github HEAD is ancestor';
+$github_remote="https://danny2p:$git_token@github.com/danny2p/dp-d91.git";
+exec("git pull $github_remote");
+exec("git push --set-upstream $github_remote");
+print "\n Pushed to remote repository.";
 
-// some debugging for workflow logs 
-print "Local: $local \n";
-print "Remote: $remote \n";
-print "$ancestor_output \n";
 
-if ($local == $remote) {
-    // in many cases CI, or user pushed to Pantheon, so Pantheon will already be up to date with Github
-    print "Up-to-date.";
-    return;
-} elseif ($is_remote_ancestor == 0) {
-    // in the case of Autopilot or dashboard one-click update, Pantheon will have new commits.  Github head should be ancestor
-    exec("git push $git_remote_auth master");
-    print "\n Pushed to Github.";
-} else {
-    print "Pantheon and Github have diverged'.";
-    // TODO - slack notification or other to notify user they may need to manually reconcile
+
+
+
+$private_files = realpath($_SERVER['HOME']."/files/private");
+$git_secrets_file = "$private_files/secrets.json";
+$git_secrets = load_git_secrets($git_secrets_file);
+$git_token = $git_secrets['token'];
+$git_remote = $git_secrets['git_remote'];
+$auth_string = "$git_token@github.com";
+
+// since we only asked for the https path and token we need to rebuild the url
+
+$path_last = strrchr($git_remote, "/");
+$git_owner = str_replace("https://github.com/", "", str_replace($path_last, "", $git_remote));
+$git_remote_auth = "https://" . $git_owner . ":" . $auth_string . "/" . $git_owner . $path_last;
+
+if (empty($git_token)) {
+    $message = "Unable to load Git token from secrets file \n";
+    print $message;
     return;
 }
+
+/*
+*
+* Since Pantheon is really authoritative, in the sense that we're running the code, 
+* we'll try to automatically push back to Github master.
+* In most cases this should be safe if commits to Github master
+* branch are always being pushed to Pantheon.
+* extend this logic as necessary to fit your needs.
+*
+*/
+
+exec("git pull $git_remote_auth");
+exec("git push --set-upstream $git_remote_auth");
+print "\n Pushed to remote repository.";
